a6d7098834a53e6fdc0098d3c2671441e85ebbe1
isaactanddoh <isaactanddoh@gmail.com>
2025-08-08 22:38:26 +0000

    new commit

---
+++ result/server.js
@@ -0,0 +1,97 @@
+var express = require('express'),
+    async = require('async'),
+    pg = require("pg"),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
+io.set('transports', ['polling']);
+
+var port = process.env.PORT || 4000;
+
+io.sockets.on('connection', function (socket) {
+
+  socket.emit('message', { text : 'Welcome!' });
+
+  socket.on('subscribe', function (data) {
+    socket.join(data.channel);
+  });
+});
+
+// Database connection configuration
+var config = {
+  user: 'postgres',
+  password: 'postgres',
+  host: 'db',
+  port: 5432,
+  database: 'postgres'
+};
+
+function connectWithRetry() {
+  console.log("Attempting to connect to database...");
+  
+  var client = new pg.Client(config);
+  
+  client.connect(function(err) {
+    if (err) {
+      console.error("Database connection error:", err.message);
+      console.log("Retrying in 1 second...");
+      setTimeout(connectWithRetry, 1000);
+      return;
+    }
+    
+    console.log("Connected to db");
+    getVotes(client);
+  });
+}
+
+// Start the connection process
+connectWithRetry();
+
+function getVotes(client) {
+  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+    if (err) {
+      console.error("Error performing query: " + err);
+    } else {
+      var votes = collectVotesFromResult(result);
+      io.sockets.emit("scores", JSON.stringify(votes));
+    }
+
+    setTimeout(function() {getVotes(client) }, 1000);
+  });
+}
+
+function collectVotesFromResult(result) {
+  var votes = {a: 0, b: 0};
+
+  result.rows.forEach(function (row) {
+    votes[row.vote] = parseInt(row.count);
+  });
+
+  return votes;
+}
+
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+});
+
+server.listen(port, function () {
+  var port = server.address().port;
+  console.log('App running on port ' + port);
+});
